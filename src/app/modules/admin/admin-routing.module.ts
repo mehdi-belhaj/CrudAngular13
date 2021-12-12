d9e8270b6e5bbc3b924f71bba583bb1e138b8e9f
EL BOUCH Mohamed <mohamedelbouch832@gmail.com>
2021-12-12 15:49:49 +0100

    add view All candidates

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { AddCandidateComponent } from './candidates/add-candidate/add-candidate.component';
-import { CandidatesComponent } from './candidates/candidates.component';
+import { CandidatesComponent } from './candidates/view-candidate/candidates.component';
 import { EditCandidateComponent } from './candidates/edit-candidate/edit-candidate.component';
 import { ImportCandidateComponent } from './candidates/import-candidate/import-candidate.component';
 
@@ -10,15 +10,18 @@ const routes: Routes = [
   {
     path: '',
     component: AdminComponent,
+    data: { title: 'Page Title' },
     children: [
       { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
       {
         path: 'candidates',
         component: CandidatesComponent,
+        data: { title: 'Page Title' },
       },
       {
         path: 'newCandidate',
         component: AddCandidateComponent,
+        data: { title: 'Page Title' },
       },
       {
         path: 'updateCandidate',
