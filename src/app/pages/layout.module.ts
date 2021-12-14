a894580a3822023c9b5e209a637f0dadb53c6f43
EL-BOUCH <mohamed.el-bouch@capgemini.com>
2021-12-14 15:03:38 +0100

    adding sweet alert and error page not found

--- src/app/pages/layout.module.ts
+++ src/app/pages/layout.module.ts
@@ -5,9 +5,10 @@ import { PagesRoutingModule } from './pages-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminRoutingModule } from '../modules/admin/admin-routing.module';
 import { CandidateRoutingModule } from '../modules/candidate/candidate-routing.module';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, ErrorComponent],
   imports: [
     CommonModule,
     PagesRoutingModule,
