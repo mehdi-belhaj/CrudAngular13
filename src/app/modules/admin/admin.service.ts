95f218c4a42810b12cd20c0017ce8223a8e83139
skywallker96 <hassan.hmiyed@capgemini.com>
2021-12-14 16:36:12 +0100

    add candidat feature

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,10 +1,16 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
 import { Candidate } from '../../models/Candidate';
 import { Page } from './candidates/view-candidate/Page';
 const API_AUTH_URL = `${environment.apiUrl}/admin`;
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+};
+
+
 @Injectable({
   providedIn: 'root',
 })
@@ -17,37 +23,14 @@ export class AdminService {
 
     return this.http.get<any>(`${API_AUTH_URL}/candidates`);
   }
-  /**
-   * Get all candidates, we use also this method to call search and filter.
-   *
-   * @param pageSize - Page size
-   * @param pageIndex - Page index
-   * @param filter - Optional value used in the filter
-   * @param search - Optional value used in the search
-   * @returns Observable<Page<Candidate>>
-   */
-  // getCandidates(
-  //   size?: number,
-  //   page: number = 0,
-  //   filter?: string,
-  //   search?: string
-  // ): Observable<Page<Candidate>> {
-  //   let returnedData: any;
-  //   let params = new HttpParams();
 
-  //   if (search) {
-  //     params = params.append('search', search.trim());
-  //   }
-  //   if (filter) {
-  //     params = params.append('filter', filter.trim());
-  //   }
-  //   if (page !== undefined && size !== undefined) {
-  //     params = params.append('page', page);
-  //     params = params.append('size', size);
-  //   }
 
-  //   return this.http.get<any>(`${API_AUTH_URL}/candidate`, {
-  //     params,
-  //   });
-  // }
+  addCandidate(candidate: Candidate): Observable<Candidate> {
+    return this.http.post<Candidate>(
+      `${API_AUTH_URL}/candidate`,
+      candidate,
+      httpOptions
+    );
+  }
+
 }
--- src/app/modules/admin/candidates/add-candidate/add-candidate.component.ts
+++ src/app/modules/admin/candidates/add-candidate/add-candidate.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Candidate } from '../../../../models/Candidate';
+import { Role } from '../../../../models/Utilisateur';
+import { AdminService } from '../../admin.service';
 
 @Component({
   selector: 'app-add-candidate',
@@ -7,9 +12,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCandidateComponent implements OnInit {
 
-  constructor() { }
+  myform!: FormGroup;
+  submitted = false;
+  errorMessage = '';
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private candidateService: AdminService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+
+    this.myform = this.formBuilder.group(
+      {
+        firstname: ['', Validators.required,Validators.minLength(3)],
+        poste: ['', Validators.required],
+        activity: ['', Validators.required],
+        lastname: ['', Validators.required,Validators.minLength(3)],
+        username: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(20),
+          ],
+        ],
+        email: ['', [Validators.required, Validators.email]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(40),
+          ],
+        ],
+      
+      },
+     
+    );
   }
+
+
+
+
+  public get Email(): FormControl {
+    return this.myform.get('email') as FormControl;
+  }
+  public get Username(): FormControl {
+    return this.myform.get('username') as FormControl;
+  }
+  public get Poste(): FormControl {
+    return this.myform.get('poste') as FormControl;
+  }
+  public get Activity(): FormControl {
+    return this.myform.get('activity') as FormControl;
+  }
+  public get Firstname(): FormControl {
+    return this.myform.get('firstname') as FormControl;
+  }
+  public get Lastname(): FormControl {
+    return this.myform.get('lastname') as FormControl;
+  }
+  public get Password(): FormControl {
+    return this.myform.get('password') as FormControl;
+  }
+
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.myform.controls;
+  }
+
+  save(): void {
+
+    if (this.myform.invalid) {
+      return;
+    }
+
+    let candidate: Candidate = {
+      firstname: this.Firstname?.value.trim().toLowerCase(),
+      lastname: this.Lastname?.value.trim().toLowerCase(),
+      username: this.Username?.value.trim().toLowerCase(),
+      email: this.Email?.value.trim().toLowerCase(),
+      poste: this.Poste?.value.trim(),
+      activityArea: this.Activity?.value.trim(),
+      password: this.Password?.value,
+      role: Role.ROLE_CANDIDAT,
+    };
+
+    console.log(candidate)
+    this.candidateService.addCandidate(candidate).subscribe(
+
+      (data) => {
+
+      
+        this.router.navigate(['/admin/candidates']);
+      },
+      (err) => {
+        this.errorMessage = err.error.message;
+      }
+    );
+
+
+  }
+
 
 }
