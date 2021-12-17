266280545f6318d33cb4ce2340c6b75b7d1ba098
skywallker96 <hassan.hmiyed@capgemini.com>
2021-12-17 12:32:30 +0100

    jzdzb

--- src/app/modules/candidate/candidate.component.ts
+++ src/app/modules/candidate/candidate.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Candidate } from '../../models/Candidate';
+import { Role } from '../../models/Utilisateur';
 import { CandidateService } from './candidate.service';
 
 @Component({
@@ -8,25 +12,115 @@ import { CandidateService } from './candidate.service';
 })
 export class CandidateComponent implements OnInit {
 
-  constructor(private candidateService : CandidateService) { }
+  myform!: FormGroup;
+  submitted = false;
+  errorMessage = '';
+
+  constructor(private candidateService : CandidateService,private formBuilder: FormBuilder,
+    private router: Router) { }
 
   candidat: any;
+  id : number;
 
   ngOnInit(): void {
     this.getCandidat();
+
+
+
+    this.myform = this.formBuilder.group(
+      {
+        firstname: ['', Validators.required],
+        poste: ['FULLSTACKDEVELOPER', Validators.required],
+        activity: ['FINANCIALSERVICES', Validators.required],
+        gender: ['MALE', Validators.required],
+
+        lastname: ['', Validators.required],
+       
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(40),
+        ],
+        ]
+
+      },
+
+    );
   }
 
 
+
+  public get Email(): FormControl {
+    return this.myform.get('email') as FormControl;
+  }
+  public get Address(): FormControl {
+    return this.myform.get('address') as FormControl;
+  }
+  public get Poste(): FormControl {
+    return this.myform.get('poste') as FormControl;
+  }
+  public get Activity(): FormControl {
+    return this.myform.get('activity') as FormControl;
+  }
+  public get Gender(): FormControl {
+    return this.myform.get('gender') as FormControl;
+  }
+  public get Firstname(): FormControl {
+    return this.myform.get('firstname') as FormControl;
+  }
+  public get Lastname(): FormControl {
+    return this.myform.get('lastname') as FormControl;
+  }
+  public get Password(): FormControl {
+    return this.myform.get('password') as FormControl;
+  }
+  public get Phone(): FormControl {
+    return this.myform.get('phone') as FormControl;
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.myform.controls;
+  }
+
+
+
+  
   getCandidat(): void {
     this.candidateService.getCandidateById()
       .subscribe(
         data => { this.candidat = data
+          this.id = data.id
          
           console.log(data);
         },
         error => {
           console.log(error);
         });
+  }
+
+
+  submit(){
+
+
+    let candidate: Candidate = {
+      firstname: this.Firstname?.value.trim().toLowerCase(),
+      lastname: this.Lastname?.value.trim().toLowerCase(),
+      email: this.Email?.value.trim().toLowerCase(),
+      address: this.Address?.value.trim().toLowerCase(),
+      phone: this.Phone?.value.trim().toLowerCase(),
+      poste: this.Poste?.value.trim(),
+      activityArea: this.Activity?.value.trim(),
+      gender: this.Gender?.value.trim(),
+      password: this.Password?.value,
+      role: Role.ROLE_CANDIDAT,
+    };
+
+  
+    this.candidateService.update(this.myform.value).subscribe(res => {
+         console.log('Candidate updated successfully!');
+         this.router.navigateByUrl('/dashboard');
+    })
   }
 
 }
--- src/app/modules/candidate/candidate.service.ts
+++ src/app/modules/candidate/candidate.service.ts
@@ -2,9 +2,11 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
+import { Candidate } from '../../models/Candidate';
 
 
 const API_AUTH_URL = `${environment.apiUrl}`;
+const API_CAND_UPDATE = `${environment.apiUrl}/candidat/personalData`;
 
 
 
@@ -19,5 +21,13 @@ export class CandidateService {
   getCandidateById(): Observable<any> {
     return this.http.get(`${API_AUTH_URL}/auth/me`);
 
+  }
+
+  //   update(id, candidate): Observable<Candidate> {
+  //   return this.http.put<Candidate>(`${API_AUTH_URL}/${id}`, candidate);
+  // }
+
+  update(candidate): Observable<Candidate> {
+    return this.http.put<Candidate>(`${API_CAND_UPDATE}`, candidate);
   }
 }
